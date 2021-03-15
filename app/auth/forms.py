36aed631da57162acb0b632b6d40dc14d4f856d6
Wahyu <ramadani.wahyu@gmail.com>
2021-03-15 13:31:17 +0900

    new

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,8 +3,22 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
+from ..models import Pegawai
+
 class LoginForm(FlaskForm):
 
     name = StringField('Nama Pengguna', validators=[DataRequired()])
     password = PasswordField('Kata Kunci', validators=[DataRequired()])
     submit = SubmitField('Masuk')
+
+class PenggunaForm(FlaskForm):
+    name = StringField('Nama Pengguna', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    pegawai = QuerySelectField('Pegawai', query_factory=lambda: Pegawai.query.all(), get_label='name', allow_blank=True)
+    submit = SubmitField('Simpan')
+
+class PasswordForm(FlaskForm):
+    old_password = PasswordField('Password Lama', validators=[DataRequired()])
+    password = PasswordField('Password Baru', validators=[DataRequired()])
+    confirm_password = PasswordField('Konfirmasi Password Baru', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Ganti Password')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import flash, redirect, render_template, url_for, request
 from flask_login import login_required, login_user, logout_user, current_user
 from . import auth
-from ..models import Pengguna
-from .forms import LoginForm
+from ..models import Pengguna, Pegawai
+from .forms import LoginForm, PenggunaForm, PasswordForm
 from .. import db
 
 
@@ -41,3 +41,41 @@ def logout():
 
     # redirect to the login page
     return redirect(url_for('auth.login'))
+
+@auth.route('/profil', methods=['GET', 'POST'])
+@login_required
+def profile():
+    id = current_user.id
+    pengguna = Pengguna.query.get_or_404(id)
+    form = PenggunaForm(obj=pengguna)
+    if form.validate_on_submit():
+        pengguna.name = form.name.data
+        pengguna.email = form.email.data
+        pengguna.pegawai = form.pegawai.data
+
+        db.session.commit()
+        flash('Data pengguna telah diubah')
+        return redirect(url_for('auth.profile'))
+    
+    form.name.data = pengguna.name
+    form.email.data = pengguna.email
+    form.pegawai.data = pengguna.pegawai
+
+    return render_template('auth/profil.html', pengguna=pengguna, form=form, title='Profil Pengguna')
+
+@auth.route('/profil/ganti-password', methods=['GET', 'POST'])
+@login_required
+def profil_password():
+    id = current_user.id
+    pengguna = Pengguna.query.get_or_404(id)
+    form = PasswordForm(obj=pengguna)
+    if form.validate_on_submit():
+        if pengguna.verify_password(form.old_password.data):
+            pengguna.password = form.password.data
+            db.session.commit()
+            flash('Password telah diganti')
+            return redirect(url_for('auth.profile'))
+        else:
+            flash('Password lama salah.')
+    
+    return render_template('auth/password.html', form=form, pengguna=pengguna, title='Ganti Password')
